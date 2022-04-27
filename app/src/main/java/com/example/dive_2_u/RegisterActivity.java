d5c897952743b06b807e8cb7ae82b4801114af5a
devkys <keongseo1234@gmail.com>
2022-04-27 15:04:21 +0900

    no_star_activity

--- app/src/main/java/com/example/dive_2_u/RegisterActivity.java
+++ app/src/main/java/com/example/dive_2_u/RegisterActivity.java
@@ -3,11 +3,19 @@ package com.example.dive_2_u;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.MotionEvent;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
     private Spinner spinner;
+
+    private TextView no_star_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,16 @@ public class RegisterActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        no_star_txt  = findViewById(R.id.no_star_txt);
+        no_star_txt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent no_star_txt = new Intent(getApplicationContext(), no_star.class);
+                startActivity(no_star_txt);
+            }
+        });
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/dive_2_u/no_star.java
@@ -0,0 +1,17 @@
+package com.example.dive_2_u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class no_star extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_star);
+    }
+
+
+}
